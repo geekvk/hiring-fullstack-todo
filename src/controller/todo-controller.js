61f664c11e5db3db7a86aa39bfab83c385ea15e2
vijanam <vijanam@mobitel.lk>
2025-11-11 14:39:04 +0530

    basic crud operation is finished

--- src/controller/todo-controller.js
+++ src/controller/todo-controller.js
@@ -3,14 +3,39 @@ const todo = require('../models/TodoItem');
 const TodoItem = require('../models/TodoItem');
 
 const getTodos = async(req, res) => {
-    
+    try{
+        const todos = await TodoItem.find({});
+        res.status(202).json({
+            success : true,
+            body : todos
+        })
+    }catch(error){
+        res.status(500).json({
+            success : false,
+            message : "Something went wrong. Please try again"
+        });
+    }
 }   
 
 const getTodoById = async(req, res) => {
     try{
-
+        const todoItemId = req.params.id;
+        const todoById = await TodoItem.findById(todoItemId);
+        if(!todoById){
+            return res.status(404).json({
+                success : false,
+                message : 'The Item is not found'
+            });
+        }
+        res.status(200).json({
+            success : true,
+            body : todoById
+        });
     }catch(error){
-
+        res.status(500).json({
+            success : false,
+            message : "Something went wrong. Please try again"
+        });
     }
 }
 
@@ -29,17 +54,87 @@ const addTodo = async(req, res) => {
     }catch(error){
         res.status(500).json({
             success : false,
-            message : error
+            message : "Something went wrong. Please try again"
         });
     }
 }
 
 const updateTodo = async(req, res) => {
+    try{
+        const itemId  = req.params.id;
+        const updatedTodo = await TodoItem.findByIdAndUpdate(
+            itemId,
+            req.body,
+            { 
+                new: true,      
+                runValidators: true
+            }
+        );
+        if (!updatedTodo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        res.status(200).json({
+            success : true,
+            body : updatedTodo
+        });
 
+    }catch(error){
+        res.status(500).json({
+            success : false,
+            message : "Something went wrong. Please try again"
+        });
+    }
+}
+
+const toggleTodo = async(req, res) => {
+    try{
+        const itemId  = req.params.id;
+        const updatedTodo = await TodoItem.findByIdAndUpdate(
+            itemId,
+            [
+                {
+                    $set: { isDone: { $not: "$isDone" } } 
+                }
+            ],
+            { new: true } 
+        );
+        if (!updatedTodo) {
+            return res.status(404).json({ message: 'Todo not found' });
+        }
+        res.status(200).json({
+            success : true,
+            body : updatedTodo
+        });
+
+    }catch(error){
+        res.status(500).json({
+            success : false,
+            message : "Something went wrong. Please try again"
+        });
+    }
 }
 
 const deleteTodo = async(req, res) => {
-    
+    try{
+        const todoItemId = req.params.id;
+        const deletedItem = await TodoItem.findByIdAndDelete(todoItemId);
+         if(!deletedItem){
+            return res.status(404).json({
+                success : false,
+                message : 'The Item is not found'
+            });
+        }
+        res.status(200).json({
+            success : true,
+            message : "Item is deleted"
+        });
+
+    }catch(error){
+        res.status(500).json({
+            success : false,
+            message : "Something went wrong. Please try again"
+        });
+    }
 }
 
-module.exports = { getTodos,getTodoById,addTodo,updateTodo,deleteTodo }
+module.exports = { getTodos,getTodoById,addTodo,updateTodo,toggleTodo, deleteTodo }
--- src/routes/todo-routes.js
+++ src/routes/todo-routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getTodos, getTodoById, addTodo, updateTodo, deleteTodo } = require('../controller/todo-controller.js');
+const { getTodos, getTodoById, addTodo, updateTodo, deleteTodo, toggleTodo } = require('../controller/todo-controller.js');
 const router = express.Router();
 
 router.get('/todos', getTodos);
 router.get('/todos/:id',getTodoById);
 router.post('/todos',addTodo);
 router.put('/todos/:id',updateTodo);
+router.patch('/todos/:id', toggleTodo)
 router.delete('/todos/:id',deleteTodo);
 
 module.exports = router;
